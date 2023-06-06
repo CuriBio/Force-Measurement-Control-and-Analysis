772d856980f0b909976224dd1edbdca6d48d37d9
kg-curi <86029660+kg-curi@users.noreply.github.com>
2023-06-06 10:50:34 -0700

    Plotting improvements

--- Hanging_Weight_Spring_Constant_Measurements.py
+++ Hanging_Weight_Spring_Constant_Measurements.py
@@ -3,6 +3,7 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import linregress
+from scipy.stats import shapiro
 
 spreadsheet_path = os.path.join("G:\Shared drives\IR&D Projects\Mantarray-V1\\3_Technical Development\Publication_Data",
                                 "Hanging Spring Constant Measurements.xlsx")
@@ -22,76 +23,103 @@ colors = [
 ]
 colors = colors + colors
 
+raw_col = np.array(ws['A:C'])
 mean_col = ws['D'] #um
 stdev_col = ws['E'] #um
 force_col = ws['G'] #uN
 
-outputs = {
-    "mean_disp_values": [],
-    "stdev_disp_values": [],
-    "force_values": []
-}
+raw_disp_values = []
+mag_nums_1x = (np.arange(5) + 1)*2
+mag_num_12x = np.arange(5) + 1
+mag_weight_1x = 2.2 * 9.8 # mg * m/s sq
+mag_weight_12x = 60.5 * 9.8 # mg * m/s sq
+
+mag_force = []
+mag_force.append(mag_nums_1x * mag_weight_1x)
+mag_force.append(mag_num_12x * mag_weight_12x)
 
 for timepoint in range(2, len(mean_col)):
-    outputs["mean_disp_values"].append(mean_col[timepoint].value)
-    outputs["stdev_disp_values"].append(stdev_col[timepoint].value)
-    outputs["force_values"].append(force_col[timepoint].value)
+    for cell in range(0, raw_col.shape[0]):
+        if type(raw_col[cell, timepoint].value) == float or type(raw_col[cell, timepoint].value) == int:
+            raw_disp_values.append(raw_col[cell, timepoint].value)
 
-for output in outputs.keys():
-    outputs[output] = np.array(outputs[output])
-    outputs[output] = outputs[output].reshape((len(outputs[output])//7, 7))[:, :5]
-    outputs[output] = outputs[output].astype(float)
+raw_disp_values = np.array(raw_disp_values).reshape(len(raw_disp_values)//15, 5, 3)
+mean_disp_values = np.mean(raw_disp_values, axis=2)
+stdev_disp_values = np.std(raw_disp_values, axis=2)
 
-# for low_post in range(7, 11):
-#     plt.errorbar(outputs["force_values"][low_post, :],
-#                  outputs["mean_disp_values"][low_post, :],
-#                  outputs["stdev_disp_values"][low_post, :],
+# ### Example position - to - force
+# for post in [0, 7]:
+#     plt.errorbar(mag_force[post//6],
+#                  mean_disp_values[post],
+#                  stdev_disp_values[post],
 #                  marker='s',
 #                  ms=5,
 #                  capsize=10,
-#                  color=colors[low_post],
+#                  color=colors[post],
 #                  fmt='_')
 #
-#     result = linregress(outputs["force_values"][low_post, :], outputs["mean_disp_values"][low_post, :])
+#     result = linregress(mag_force[post//6], mean_disp_values[post])
 #     print(" R Sq.")
 #     print(result.rvalue ** 2)
 #     print(" Stiffness ")
 #     print(result.slope**-1)
-#     plt.plot(outputs["force_values"][low_post,:],
-#              result.slope*outputs["force_values"][low_post, :] + result.intercept, color=colors[low_post])
-
-def plot_f2p(post_range, color):
-    mean_force = np.mean(outputs["force_values"][post_range, :], axis=0)
-    mean_disp = np.mean(outputs["mean_disp_values"][post_range, :], axis=0)
-    stdev_disp = np.std(outputs["mean_disp_values"][post_range, :], axis=0)
-
-    plt.errorbar(mean_force,
-                 mean_disp,
-                 stdev_disp,
-                 marker='s',
-                 ms=5,
-                 capsize=10,
-                 color=color,
-                 fmt='_')
-
-    result = linregress(mean_force, mean_disp)
-    print(" R Sq.")
-    print(result.rvalue**2)
-    print(" Stiffness ")
-    print(result.slope**-1)
-    plt.plot(mean_force,
-             result.slope*mean_force + result.intercept, color=color)
+#     plt.plot(mag_force[post//6],
+#              result.slope*mag_force[post//6] + result.intercept, color=colors[post])
 #
-# plot_f2p(slice(0, 6), "blue")
-plot_f2p(slice(7, 11), "orange")
-# plt.legend(["Low Stiffness Post", "High Stiffness Post"])
-plt.title("high stiffness post")
-plt.ylabel("post head displacement (\u03BCm)")
-plt.xlabel("applied force (\u03BCN)")
-plt.grid(True)
-plt.savefig(r"G:\Shared drives\IR&D Projects\Mantarray-V1\3_Technical Development\Publication_Data" + "\\" + "high_stiffness_post")
+# plt.ylabel("post head displacement (\u03BCm)")
+# plt.xlabel("applied force (\u03BCN)")
+# plt.legend(["low stiffness", "high stiffness"])
+# plt.grid(True)
+# plt.savefig(r"G:\Shared drives\IR&D Projects\Mantarray-V1\3_Technical Development\Publication_Data" + "\\" + "post_stiffness_example")
+# plt.show()
 
+
+
+spring_constants_1x = []
+spring_constants_12x = []
+
+### Example position - to - force
+for post in range(0, 11):
+    result = linregress(mag_force[post // 6], mean_disp_values[post])
+    print(" R Sq.")
+    print(result.rvalue ** 2)
+    print(" Stiffness ")
+    print(result.slope ** -1)
+
+    if (post < 6):
+        spring_constants_1x.append(result.slope**-1)
+    elif (post > 6):
+        spring_constants_12x.append(result.slope**-1)
+
+["low stiffness", "high stiffness"]
+# plt.figure(figsize=(5, 6))
+bar_positions = [.375, .625]
+plt.margins(.125)
+plt.bar(bar_positions,
+             [np.mean(spring_constants_1x), np.mean(spring_constants_12x)],
+             yerr=[2*np.std(spring_constants_1x), 2*np.std(spring_constants_12x)],
+             capsize=10,
+        tick_label=["low stiffness", "high stiffness"],
+        edgecolor="black",
+        facecolor="gainsboro",
+        color="white",
+        width=.2)
+plt.xticks(bar_positions)
+plt.plot(bar_positions[0] * np.ones(len(spring_constants_1x)) + .1 * np.random.rand(len(spring_constants_1x)) - .05,
+         spring_constants_1x, "x", color="black")
+plt.plot(bar_positions[1] * np.ones(len(spring_constants_12x)) + .1 * np.random.rand(len(spring_constants_12x)) - .05
+         , spring_constants_12x, "x", color="black")
+
+
+plt.ylabel("stiffness (N/m)")
+plt.grid(True)
+plt.savefig(r"G:\Shared drives\IR&D Projects\Mantarray-V1\3_Technical Development\Publication_Data" + "\\" + "spring constant variability")
 plt.show()
 
+print(spring_constants)
 
-print(output)
+# Test Normality of each timepoint
+stat, p = shapiro(spring_constants)
+print(p)
+
+
