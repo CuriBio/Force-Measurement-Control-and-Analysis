1d0c06587bd379236edc1877a60f09f9d3bc6585
kg-curi <86029660+kg-curi@users.noreply.github.com>
2023-12-06 00:08:32 -0800

    Accommodate single cycle stiffness

--- Hanging_Weight_Spring_Constant_Measurements.py
+++ Hanging_Weight_Spring_Constant_Measurements.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 from scipy.stats import linregress
 from scipy.stats import shapiro
 
-spreadsheet_path = os.path.join(r"C:\Users\kevin\OneDrive\Documents\weight_hanging_test_set_1.xlsx")
+spreadsheet_path = os.path.join(r"G:\Shared drives\IR&D Projects\Mantarray-V1\3_Technical Development\MAv1_Field-to-Force\Position To Force\Spring Constant Estimates -- Hanging Weight\spring_constant_test_11252022\weight_hanging_test_set_1.xlsx")
 print(spreadsheet_path)
 
 wb = xl.load_workbook(spreadsheet_path, data_only=True)
@@ -14,8 +14,11 @@ ws = wb.active
 raw_col = np.array(ws['A'])
 raw_disp_values_list = []
 weight_num_12x = np.arange(6)
-weight_12x = .000050 * 9.8 # kg * m/s sq
+weight_12x = .000050*9.8 # kg * m/s sq
 scale = 71000 # pixels / m
+num_posts = 5
+num_cycles = 4
+num_weights = len(weight_num_12x)
 
 force = weight_num_12x * weight_12x
 
@@ -23,23 +26,42 @@ for cell in range(0, len(raw_col)):
     if type(raw_col[cell].value) == float or type(raw_col[cell].value) == int:
         raw_disp_values_list.append(raw_col[cell].value)
 
-raw_disp_values = np.array(raw_disp_values_list).reshape(5, 4, 6) / scale
-mean_disp_values = np.mean(raw_disp_values.T - raw_disp_values[:, :, 0].T, axis=1).T
-stdev_disp_values = np.std(raw_disp_values.T - raw_disp_values[:, :, 0].T, axis=1).T
+# absolute pixel position of post head
+position_values = np.array(raw_disp_values_list).reshape(num_posts, num_cycles, num_weights)/scale
+# change in position from unweighted position
+displacement_values = (position_values.T - position_values[:, :, 0].T).T
+# mean change in unweighted position
+mean_disp_values = np.mean(displacement_values, axis=1)
+# standard deviation of change in unweighted position
+stdev_disp_values = np.std(displacement_values, axis=1)
 
-### Example position - to - force
+# Example position - to - force
 for post in range(0, 5):
-    plt.errorbar(force,
-                 mean_disp_values[post],
-                 stdev_disp_values[post],
-                 marker='s',
-                 ms=5,
-                 capsize=10,
-                 fmt='_')
 
-    result = linregress(force, mean_disp_values[post])
+    ## plot mean displacements
+    # plt.errorbar(
+    #     force,
+    #     mean_disp_values[post],
+    #     stdev_disp_values[post],
+    #     marker='s',
+    #     ms=5,
+    #     capsize=10,
+    #     fmt='_'
+    # )
+    #
+    # result = linregress(force, mean_disp_values[post, 0])
+
+    # plot displacements from one loading cycle
+    plt.plot(
+        force,
+        displacement_values[post, 0],
+        "x"
+    )
+
+    result = linregress(force, displacement_values[post, 0])
+
     print(" R Sq.")
-    print(result.rvalue ** 2)
+    print(result.rvalue**2)
     print(" Stiffness ")
     print(result.slope**-1)
     plt.plot(force,
@@ -54,14 +76,29 @@ for post in range(0, 5):
 
 spring_constants_12x = []
 
-### Example position - to - force
+# Plot all regression slopes
 for post in range(0, 5):
-    result = linregress(force, mean_disp_values[post])
+    # result = linregress(force, mean_disp_values[post]) # mean displacement of all cycles
+    result = linregress(force, displacement_values[post, 0]) # one cycle
+
     print(" R Sq.")
-    print(result.rvalue ** 2)
+    print(result.rvalue**2)
     print(" Stiffness ")
-    print(result.slope ** -1)
+    print(result.slope**-1)
     spring_constants_12x.append(result.slope**-1)
+
+k_mean = np.mean(spring_constants_12x)
+k_2stdev = 2*np.std(spring_constants_12x)
+print("mean stiffness:")
+print(k_mean)
+print("2x standard deviation stiffness")
+print(k_2stdev)
+print("as a percentage of mean stiffness")
+print(k_2stdev/k_mean * 100)
+
+print("Shapiro-Wilk p-value")
+stat, p = shapiro(spring_constants_12x)
+print(p)
 
 bar_positions = [1]
 plt.margins(.125)
@@ -75,18 +112,17 @@ plt.bar(bar_positions,
         color="white",
         width=.2)
 plt.xticks(bar_positions)
-plt.plot(bar_positions * np.ones(len(spring_constants_12x)) + .1 * np.random.rand(len(spring_constants_12x)) - .05
-         , spring_constants_12x, "x", color="black")
+for k in range(0, len(spring_constants_12x)):
+    plt.plot(bar_positions[0]
+             + .1*np.random.rand()
+             - .05,
+             spring_constants_12x[k], marker="${0}$".format(k + 1), color="black")
 
 
 plt.ylabel("stiffness (N/m)")
 plt.grid(True)
 plt.show()
 
-print(np.mean(spring_constants_12x))
 
-# Test Normality of each timepoint
-stat, p = shapiro(spring_constants_12x)
-print(p)
 
 
